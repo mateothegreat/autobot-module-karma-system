5119b6e8520139b478ee949b056ed5f3cbd82edf
Matthew Davis <matthew@matthewdavis.io>
2019-03-21 08:21:08 -0500

    first commit

---
+++ src/Commands/KarmaLeaderboardCommand.ts
@@ -0,0 +1,57 @@
+import { Command, CommandBase, CommandParser, DB, Event } from '@autobot/common';
+import { RichEmbed }                                      from 'discord.js';
+import { KarmaPoint }                                     from '../DB/KarmaPoint';
+
+/**
+ * Displays the karma leaderboard.
+ */
+@Command
+export class KarmaLeaderboardCommand extends CommandBase {
+
+    public constructor() {
+
+        //
+        // Set this commands configuration.
+        //
+        super({
+
+            event: Event.MESSAGE,
+            name: '!leaderboard',
+            group: 'fun',
+            description: 'Displays the karma leaderboard.',
+            entities: [ KarmaPoint ]
+
+        });
+
+    }
+
+    /**
+     * Called when a command matches config.name.
+     *
+     * @param command Parsed out commamd
+     *
+     */
+    public async run(command: CommandParser) {
+
+        const results = await DB.connection.getRepository(KarmaPoint)
+                                .createQueryBuilder('karma_point')
+                                .select([ 'to_userid', 'to_discriminator', 'to_username', 'COUNT(karma_point.id) AS total' ])
+                                .orderBy('total', 'DESC')
+                                .groupBy('to_userid,to_discriminator,to_username')
+                                .limit(22)
+                                .getRawMany();
+
+        const embed = new RichEmbed().setTitle('Karma Points Leaderboard')
+                                     .setColor(3447003);
+
+        results.forEach(row => {
+
+            embed.addField(`❯ ${ row.total } points`, `<@${ row.to_userid }>`);
+
+        });
+
+        command.obj.channel.send(embed);
+
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { Command, CommandBase, CommandParser, Event } from '@autobot/common';
+import { RichEmbed }                                  from 'discord.js';
+
+/**
+ * Demonstrates a simple command that uses arguments.
+ *
+ * Example message: `>test chars=abc,num=123,opt=hellow world`
+ */
+@Command
+export class TestCommand extends CommandBase {
+
+    public constructor() {
+
+        //
+        // Set this commands configuration.
+        //
+        super({
+
+            event: Event.MESSAGE,
+            name: '>test',
+            group: 'testing',
+            description: 'Simple test command that sends a reply if validation succeeds.',
+            params: [ {
+
+                name: 'chars',
+                description: 'An argument containing a word.',
+                required: true,
+                pattern: '[a-zA-Z]+'
+
+            }, {
+
+                name: 'num',
+                description: 'An argument containing numbers.',
+                required: true,
+                pattern: '\\d+'
+
+            }, {
+
+                name: 'opt',
+                description: 'An optional argument.',
+                required: false,
+
+            } ]
+
+        });
+
+    }
+
+    /**
+     * Called when a command matches config.name.
+     *
+     * @param command Parsed out commamd
+     *
+     */
+    public run(command: CommandParser): void {
+
+        command.obj.reply(new RichEmbed().setTitle('Test received!')
+                                         .setDescription(`argument 1: ${ command.arguments[ 0 ].name }=${ command.arguments[ 0 ].value }\nargument 2: ${ command.arguments[ 1 ].name }=${ command.arguments[ 1 ].value }`));
+
+    }
+
+}
